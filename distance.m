0ee9457e4bb9c66d786a5145c7fed9720651d329
Cervedin <Erik@CervinEd.in>
2017-11-21 20:22:02 +0100

    Added missing functions and project description to each part
    All explicitly required functions have been added to the project as well as a full description of each of the remaining parts

---
+++ distance.m
@@ -0,0 +1,16 @@
+function x = distance(T, route)
+%DISTANCE Summary of this function goes here
+%   Detailed explanation goes here
+
+end
+
+% Sträcka: Antag nu att föraren vill veta hur långt längs rutten hon
+% förväntas ommitpåenvisstid. Utgå
+% frånekvationenförankomsttidochformuleradenicke-linjäraekvationvarsrotgerförväntadtillryggalagdsträcka
+% efter T timmar. Formulera sedan Newtons metod för att lösa ekvationen.
+% Fundera på vad som skulle vara en bra startgissning för att hitta roten.
+% Skriv en matlab-funktion där du implementerar Newtons metod inklusive
+% startgissning. Funktionen skall ha funktionshuvudet function x =
+% distance(T, route) Hur långt kommer Anna och Elsa längs sina respektive
+% rutter på 30 min? Tips: Använd funktionerna time_to_destination och
+% velocity.
---
+++ reach.m
@@ -0,0 +1,17 @@
+function x = reach(C, route)
+%REACH Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+end
+
+% Räckvidd: Antag nu att batteriet är laddat till C Wh. Formulera den
+% ickelinjäraekvationvarsrotgerbilensräckvidd.FormulerasedanNewtonsmetod
+% för att lösa ekvationen. Fundera på vad som skulle vara en bra
+% startgissning för att hitta roten. Skriv en matlab-funktion där du
+% implementerar
+% Newtonsmetodinklusivestartgissning.Funktionenskallhafunktionshuvudet
+% function x = reach(C, route) Hur långt skulle Anna och Elsa komma på sina
+% respektive rutter med en batteriladdning på C = 10000 Wh? Tips: Använd
+% funktionerna total_consumption, velocity och consumption
