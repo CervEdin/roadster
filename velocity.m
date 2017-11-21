1c8e764756953e988f7241f045b290af2cd1e95c
Cervedin <Erik@CervinEd.in>
2017-11-21 17:08:08 +0100

    Removed old unused code in comments

--- velocity.m
+++ velocity.m
@@ -14,21 +14,6 @@ end
 
 assert( x >= 0 && x <= scope.distance_km(length(scope.distance_km)));
 
-%plot(scope.distance_km, scope.speed_kmph)
-
-%i = 1;
-%while( x > scope.distance_km(i) && x > scope.distance_km(i + 1))
-%    i = i + 1;
-% end
-% x_values = [scope.distance_km(i), scope.distance_km(i + 1)];
-% y_values = [scope.speed_kmph(i), scope.speed_kmph(i + 1)];
-% polynom = polyfit(x_values, y_values, 1);
-%v = polyval(polynom, x);
-
-% polynom = spline(scope.distance_km, scope.speed_kmph)
-% 
-% v = ppval(polynom, x);
-
 v = spline(scope.distance_km, scope.speed_kmph, x);
 
 end
