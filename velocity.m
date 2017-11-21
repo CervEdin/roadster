9382f2c3b0dba00fb05c2e7b3fe7d4ea3d90e64f
Cervedin <Erik@CervinEd.in>
2017-11-21 17:06:12 +0100

    Changed loading data files from using string input in function to case switching using if/else loop

--- velocity.m
+++ velocity.m
@@ -3,18 +3,14 @@ function v = velocity(x, route)
 % x is the distance travelled along a route
 % route is a string for the route file 'speed_anna' or 'speed_elsa'
 
+% Check the route and load appropriate data
 if strcmp(route, 'speed_anna')
-    
+    scope = load('speed_anna.mat');
 elseif strcmp(route, 'speed_elsa')
-    
+    scope = load('speed_elsa.mat');
 else
     error("Route has to be either 'speed_anna' or 'speed_elsa'")
 end
-
-% load(filename '-mat')
-filename = strcat(route, '.mat');
-
-scope = load(filename);
 
 assert( x >= 0 && x <= scope.distance_km(length(scope.distance_km)));
 
