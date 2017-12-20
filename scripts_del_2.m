07f06f51f337e33439151e91b209b39bcc127aa7
Cervedin <Erik@CervinEd.in>
2017-12-20 15:46:49 +0100

    Add plots of Anna and Elsa using time_to_distance_simpson on a small interval close to zero

--- scripts_del_2.m
+++ scripts_del_2.m
@@ -5,6 +5,14 @@ anna.string = 'speed_anna';
 elsa = load('speed_elsa.mat');
 elsa.string = 'speed_elsa';
 
-test_graph.x = linspace(0, max(anna.distance_km));
-test_graph.y = arrayfun( @(x) time_to_destination_simpson(x, anna.string, 256), test_graph.x);
-plot(test_graph.x, test_graph.y);   
+test_graph.x = linspace(0, 2, 1000);
+test_graph.y = arrayfun( @(x) time_to_destination_simpson(x, anna.string, 2^16), test_graph.x) .* 60;
+plot(test_graph.x, test_graph.y); 
+xlabel("Distance (km)")
+ylabel("Time (minutes)")
+hold on
+
+test_graph.x = linspace(0, 2, 1000);
+test_graph.y = arrayfun( @(x) time_to_destination_simpson(x, elsa.string, 2^16), test_graph.x) .* 60;
+plot(test_graph.x, test_graph.y);
+title("Time elapsed per kilometer")
