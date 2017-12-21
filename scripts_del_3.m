837ad98f4a58a6efa37389115950ade501f46a23
erik <erik@vader>
2017-12-21 13:51:58 +0100

    Added test for reach function

--- scripts_del_3.m
+++ scripts_del_3.m
@@ -28,11 +28,22 @@ elsa.string = 'speed_elsa';
 % ylabel('Distance (km)')
 % title('Elsa distance over time')
 
-test_graph.x = linspace(0, max(anna.distance_km));
-test_graph.y = arrayfun(@(x) total_consumption(x, anna.string, 2^16), test_graph.x);
-plot(test_graph.x, test_graph.y);
+% test_graph.x = linspace(0, max(anna.distance_km));
+% test_graph.y = arrayfun(@(x) total_consumption(x, anna.string, 2^16), test_graph.x);
+% plot(test_graph.x, test_graph.y);
+% hold on
+% test_graph.x = linspace(0, max(elsa.distance_km));
+% test_graph.y = arrayfun(@(x) total_consumption(x, elsa.string, 2^16), test_graph.x);
+% plot(test_graph.x, test_graph.y);
+
+% Test reach function and plot values
+test_graph.x = linspace(-1, total_consumption(max(anna.distance_km), anna.string, 2^16) + 1);
+test_graph.y = arrayfun(@(x) reach(x, 'speed_anna'), test_graph.x);
+plot(test_graph.x, test_graph.y)
 hold on
-test_graph.x = linspace(0, max(elsa.distance_km));
-test_graph.y = arrayfun(@(x) total_consumption(x, elsa.string, 2^16), test_graph.x);
-plot(test_graph.x, test_graph.y);
-%test_graph.x = linspace(0, 
+test_graph.x = linspace(-1, total_consumption(max(elsa.distance_km), elsa.string, 2^16) + 1);
+test_graph.y = arrayfun(@(x) reach(x, 'speed_elsa'), test_graph.x);
+plot(test_graph.x, test_graph.y)
+xlabel('Consumption (Wh)')
+ylabel('Distance (km)')
+title('Reach per charge')
