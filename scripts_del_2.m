ce0f47b73125c8e63de2c6939600a51e3ae4b883
erik <erik@vader>
2017-12-21 16:17:00 +0100

    Updated script to test and plot integral functions for part 2

--- scripts_del_2.m
+++ scripts_del_2.m
@@ -1,4 +1,5 @@
-clear;
+clear
+clf
 
 % Load data from disk
 load roadster.mat
@@ -7,61 +8,36 @@ anna.string = 'speed_anna';
 elsa = load('speed_elsa.mat');
 elsa.string = 'speed_elsa';
 
-subplot(2, 2, 1);
-test_graph.x = linspace(0, 0.6);
+% Test and plot the time_to_destination function
+subplot(1, 2, 1);
+x_min = 0;
+x_max = anna.distance_km(end);
+test_graph.x = linspace(x_min, x_max);
 test_graph.y = arrayfun( @(x) time_to_destination(x, anna.string, 2^16), test_graph.x) .* 60;
 plot(test_graph.x, test_graph.y);
 hold on
 
+x_max = elsa.distance_km(end);
+test_graph.x = linspace(x_min, x_max);
 test_graph.y = arrayfun( @(x) time_to_destination(x, elsa.string, 2^16), test_graph.x) .* 60;
 plot(test_graph.x, test_graph.y);
 title("Time elapsed per kilometer")
 xlabel("Distance (km)")
 ylabel("Time (minutes)")
 
-subplot(2, 2, 2);
-test_graph.y = arrayfun( @(x) velocity(x, anna.string), test_graph.x);
-plot(test_graph.x, test_graph.y); 
-title("Speed at distance along the route")
-xlabel("Distance (km)")
-ylabel("Velocity (km/h)")
-hold on
-
-test_graph.y = arrayfun( @(x) velocity(x, elsa.string), test_graph.x);
+% Test and plot the total_consumption function
+subplot(1, 2, 2);
+x_min = 0;
+x_max = anna.distance_km(end);
+test_graph.x = linspace(x_min, x_max);
+test_graph.y = arrayfun( @(x) total_consumption(x, anna.string, 2^16), test_graph.x) .* 60;
 plot(test_graph.x, test_graph.y);
+hold on
 
-subplot(2, 2, 3);
-T = 0.05;
-fx = @(x, T) time_to_destination(x, anna.string, 2.^16) - T;
-test_graph.y = arrayfun(@(x) fx(x, T), test_graph.x);
-plot(test_graph.x, test_graph.y)
-hold on
-T = 0.04;
-test_graph.y = arrayfun(@(x) fx(x, T), test_graph.x);
-plot(test_graph.x, test_graph.y)
-hold on
-T = 0.03;
-test_graph.y = arrayfun(@(x) fx(x, T), test_graph.x);
-plot(test_graph.x, test_graph.y)
-hold on
-T = 0.02;
-test_graph.y = arrayfun(@(x) fx(x, T), test_graph.x);
-plot(test_graph.x, test_graph.y)
-hold on
-T = 0.01;
-test_graph.y = arrayfun(@(x) fx(x, T), test_graph.x);
-plot(test_graph.x, test_graph.y)
-hold on
-T = 0;
-test_graph.y = arrayfun(@(x) fx(x, T), test_graph.x);
-plot(test_graph.x, test_graph.y)
-title("Plotting f(x) - T = 0 for T -> 0")
+x_max = elsa.distance_km(end);
+test_graph.x = linspace(x_min, x_max);
+test_graph.y = arrayfun( @(x) total_consumption(x, elsa.string, 2^16), test_graph.x) .* 60;
+plot(test_graph.x, test_graph.y);
+title("Watts consumed per kilometer")
 xlabel("Distance (km)")
-ylabel("f(x) - T")
-
-subplot(2, 2, 4);
-title("1 / f'(x)")
-fx = @(x, route) 1 ./ velocity(x, route);
-fplot( @(x) fx(x, anna.string), [0 0.6] );
-hold on
-fplot( @(x) fx(x, elsa.string), [0 0.6] );
+ylabel("Energy consumed (Wh)")
